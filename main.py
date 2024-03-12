1a1541fc148cf882e07776c3cd7a671740a9e7ff
Matt Robinson <robinson.2416@buckeyemail.osu.edu>
2024-03-11 21:22:30 -0500

    Init Flask Framework

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/api/v1":
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            message = "Hello World"
-            self.wfile.write(bytes(message, "utf8"))
+app = Flask(__name__)
+
+@app.route('/api/v1')
+def home():
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
-    wserver = HTTPServer(('localhost', 8080), MyServer)
-    print("Server started on http://localhost:8080")
-    
-    try:
-        wserver.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    wserver.server_close()
-    print("Server Stopped")
+    app.run("localhost", 8080, debug=True)
