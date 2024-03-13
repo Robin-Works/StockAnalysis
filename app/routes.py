cf36c4b6121c46795303b0c784c6eea03cd3d300
Matt Robinson <robinson.2416@buckeyemail.osu.edu>
2024-03-12 20:44:06 -0500

    HTML templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 # Decorators which define the app route, both decorate the index function
 @app.route("/api/v1")
+def apiV1():
+    return "This will do something at some point I hope"
+
 @app.route("/index")
 def index():
-    return "Hello World Again"
+    user = {"username": "matthew"}
+    # uses the Jinja template engine that comes with flask
+    return render_template("index.html", user=user)
