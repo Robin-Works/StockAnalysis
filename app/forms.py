bd81b998eacb58b382301994a8c74f8e76d8aa46
Matt Robinson <robinson.2416@buckeyemail.osu.edu>
2024-03-14 21:44:15 -0500

    Register user

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sqlA
+from app import db
+from app.models import User
 
 class LoginForm(FlaskForm):
     # DataRequired validator just validates field is not empty
@@ -8,3 +11,21 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+    
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()]) # Email() ensures input matches input of an actual email
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")]) # EqualTo() does what you'd expect
+    submit = SubmitField("Register")
+    
+    # WTForms will invoke these in addition to stock validators, true for any function with name validate_<name>
+    def validate_username(self, username):
+        user = db.session.scalar(sqlA.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError("Username Taken! Please try something else.")
+        
+    def validate_email(self, email):
+        user = db.session.scalar(sqlA.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError("Email already used! Please use a different email address.")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sqlA
 from app import db
@@ -23,6 +23,24 @@ def index():
     ]
     # uses the Jinja template engine that comes with flask
     return render_template("index.html", title="Home Page", posts=posts)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Create new user from form input data
+        user = User(username=form.username.data, email=form.email.data)
+        user.setPassword(form.password.data)
+        
+        # Add new user to db, double buffer type deal
+        db.session.add(user)
+        db.session.commit()
+        
+        flash("Congratulations, {}, you are now registered!".format(user.username))
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
